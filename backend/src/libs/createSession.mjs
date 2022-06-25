1fa1172241bc39dea7f336269ffcf0351b04c9e9
Adrian Helvik <adrianhelvik100@gmail.com>
2022-06-25 22:39:08 +0200

    More complex session id

--- backend/src/libs/createSession.mjs
+++ backend/src/libs/createSession.mjs
@@ -1,8 +1,9 @@
+import random from './random.mjs'
 import uuid from '../uuid.mjs'
 import knex from './db.mjs'
 
 export default async function createSession(userId) {
-  const sessionId = uuid()
+  const sessionId = await random()
 
   await knex('sessions').insert({
     sessionId,
